b7ace4dc92e44062cbac9d6b20ac3899911273aa
ElenaLGM7 <168128552+ElenaLGM7@users.noreply.github.com>
2025-10-20 19:43:14 +0200

    Refine error message for DATABASE_URL loading
    Updated error message for missing DATABASE_URL in .env.

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,14 @@
-# backend/app/database.py
 from sqlmodel import SQLModel, create_engine
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # carga DATABASE_URL desde .env
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL no está definido en .env")
+    raise RuntimeError("DATABASE_URL no encontrada en .env")
 
-# create_engine de SQLModel (SQLAlchemy)
+# create_engine manejará la conexión a Postgres (Neon)
 engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 
 def create_db_and_tables():
