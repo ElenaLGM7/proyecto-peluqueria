9553d1c057ad32285f9793563c5ca5071d16a3aa
Elena Lucía Guardia Martínez <168128552+ElenaLGM7@users.noreply.github.com>
2025-10-20 22:16:51 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,13 @@
-from sqlmodel import SQLModel, create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL no encontrada en .env")
 
-# create_engine manejará la conexión a Postgres (Neon)
-engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
